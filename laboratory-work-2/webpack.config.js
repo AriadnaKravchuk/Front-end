16e8bd6c923478d1c62a307698f3cbff4ac65a87
unknown <asdfgh.qa198@gmail.com>
2023-03-08 13:06:46 +0200

    Change configuration

--- laboratory-work-2/webpack.config.js
+++ laboratory-work-2/webpack.config.js
@@ -5,8 +5,12 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     mode: 'development',
+    entry: { 
+        main: path.resolve(__dirname, './src/index.js'), 
+    },
     output: {
         path: path.resolve(__dirname, "./dist"),
+        filename: '[name].bundle.js'
     },
     devServer: {
         static: {
